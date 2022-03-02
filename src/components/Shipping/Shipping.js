84fd7cff3d8fe45222f5a6825ff9775906384d06
Marcel Lukomski <marcel.lukomski@gmail.com>
2022-03-02 19:39:47 +0100

    Add shipping form.

--- src/components/Shipping/Shipping.js
+++ src/components/Shipping/Shipping.js
@@ -14,14 +14,66 @@ const Shipping = () => {
           </Col>
         </Row>
         <Row>
+          <Col xs={12}>
+            <div className='delivery flexible'>
+              <label htmlFor='delivery'>SELECT DELIVERY METHOD</label>
+              <select id='delivery'>
+                <option value='dhl'>DHL</option>
+                <option value='ups'>UPS</option>
+                <option value='dpd'>DPD</option>
+                <option value='inpost'>InPost</option>
+              </select>
+            </div>
+          </Col>
+        </Row>
+        <Row>
           <Col>
-            <label htmlFor='delivery'>SELECT DELIVERY METHOD</label>
-            <select id='delivery'>
-              <option value='dhl'>DHL</option>
-              <option value='ups'>UPS</option>
-              <option value='dpd'>DPD</option>
-              <option value='inpost'>InPost</option>
-            </select>
+            <div className='firstname flexible'>
+              <label htmlFor='firstname'>FIRST NAME</label>
+              <input id='firstname' name='firstname' type='text' />
+            </div>
+          </Col>
+          <Col>
+            <div className='lastname flexible'>
+              <label htmlFor='lastname'>LAST NAME</label>
+              <input id='lastname' name='lastname' type='text' />
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12}>
+            <div className='address flexible'>
+              <label htmlFor='address'>ADDRESS</label>
+              <input id='address' name='address' type='text' />
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div className='city flexible'>
+              <label htmlFor='city'>CITY</label>
+              <input id='city' name='city' type='text' />
+            </div>
+          </Col>
+          <Col>
+            <div className='postal flexible'>
+              <label htmlFor='postal'>POSTAL CODE</label>
+              <input id='postal' name='postal' type='number' />
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div className='phone flexible'>
+              <label htmlFor='phone'>PHONE NUMBER</label>
+              <input id='phone' name='phone' type='number' />
+            </div>
+          </Col>
+          <Col>
+            <div className='email flexible'>
+              <label htmlFor='email'>E-MAIL</label>
+              <input id='email' name='email' type='email' />
+            </div>
           </Col>
         </Row>
       </Container>
